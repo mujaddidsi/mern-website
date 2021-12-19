ef878d3da1ee12aa252b2e65d174a988e3f0ae58
mujaddidsi <mujaddid.s.i@hotmail.com>
2021-12-19 21:44:42 +0700

    navbar + hero

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import 'assets/scss/style.scss';
+import LandingPage from 'pages/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
